937d978d627cef9e17c1ea97b78b5c03db13f6fa
NSF9 <alzhraniii999@gmail.com>
2026-02-18 00:36:00 +0300

    Update to_kv function: allow float values and return as tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,8 +5,13 @@
 Docstring for python_variable_annotations.7-to_kv
 """
 
-def to_kv(k: str, v: int) -> tuple:
+
+from typing import Union, Tuple
+
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a tuple of the key and value
     """
-    return (k, v)
+    return (k, float(v))
