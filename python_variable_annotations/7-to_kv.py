4f6879b88adb7e9a05bee29ca3dc7cc20c0b562e
NSF9 <alzhraniii999@gmail.com>
2026-02-18 00:33:36 +0300

    Add 7-to_kv.py: implement to_kv function to return a tuple of key and value

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+"""
+Docstring for python_variable_annotations.7-to_kv
+"""
+
+def to_kv(k: str, v: int) -> tuple:
+    """
+    Returns a tuple of the key and value
+    """
+    return (k, v)
