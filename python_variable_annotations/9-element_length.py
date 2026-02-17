9496d5e27d7769768ad23c82304490fc1377eb7c
NSF9 <alzhraniii999@gmail.com>
2026-02-18 00:50:40 +0300

    Add element_length function to return elements and their lengths from a list of sequences

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Module that contains the function element_length which takes a list of sequences and returns
+a list of tuples containing the elements of the input list and their lengths."""
+
+from typing import Iterable, Sequence, List, Tuple
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples containing the elements of the input list and their lengths."""
+    return [(i, len(i)) for i in lst]
