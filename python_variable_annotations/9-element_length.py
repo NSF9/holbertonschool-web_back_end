a17f484b193ed5b5cad8a5091f3fcb5a9f1ec57a
NSF9 <alzhraniii999@gmail.com>
2026-02-18 01:02:35 +0300

    Refactor docstring in 9-element_length.py for clarity and conciseness

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-""" Module that contains the function element_length which takes a list of sequences"""
+""" Module that contains the function element_length"""
 
 
 from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
-    """Returns a list of tuples containing the elements of the input list and their lengths"""
+    """Returns a list of tuples containing the elements and their lengths"""
     return [(i, len(i)) for i in lst]
