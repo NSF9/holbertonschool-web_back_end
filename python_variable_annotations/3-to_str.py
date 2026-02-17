239b399fa6fe9a1cb8406a0dd7d1e8e69edb4da9
Nawaf <Alzhraniii999@gmail.com>
2026-02-17 17:36:24 +0300

    Add to_str function implementation with annotations and main test script

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""module for to_str function with annotations"""
+
+
+def to_str(n: float) -> str:
+    """returns the string representation of a float"""
+    return str(n)
