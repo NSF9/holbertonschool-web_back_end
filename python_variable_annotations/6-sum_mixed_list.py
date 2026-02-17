956edb41163cba1e323847b867a1c8b6ab4d466b
NSF9 <alzhraniii999@gmail.com>
2026-02-18 00:28:57 +0300

    Refactor sum_mixed_list module: update docstring and clean up imports

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
+"""Module for summing a mixed list of integers and floats."""
 
-
-from ctypes import Union
-from rpds import List
-
+from typing import List, Union
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """Sums a list of integers and floats."""
 
+    
     return float(sum(mxd_lst))
