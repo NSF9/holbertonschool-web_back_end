7bad7e49d028f03eebdfb2e13cee8a446e8b6d37
NSF9 <alzhraniii999@gmail.com>
2026-02-18 00:43:10 +0300

    Fix typo in 7-main.py and add 8-main.py and 8-make_multiplier.py files with function implementation

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+
+""" Annotate the make_multiplier function with the correct types."""
+
+
+def make_multiplier(multiplier: float)-> float:
+    """ Returns a function that multiplies a float by multiplier."""
+    return (multiplier * multiplier)
