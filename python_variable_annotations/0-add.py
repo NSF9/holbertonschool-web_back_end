007dab9af46b7819e2fdf75ac21785d37813e9c6
Nawaf <Alzhraniii999@gmail.com>
2026-02-17 17:23:47 +0300

    Fix formatting by adding newlines in 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """module for add function with annotations"""
+
+
 def add(num1: float, num2: float) -> float:
     """returns the sum of num1 and num2"""
     return num1 + num2
