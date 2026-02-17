6bfefde98b041c69a9d8c36b7070d233982a4db2
NSF9 <alzhraniii999@gmail.com>
2026-02-18 00:38:19 +0300

    Remove unnecessary blank line in to_kv function

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -9,7 +9,6 @@ Docstring for python_variable_annotations.7-to_kv
 from typing import Union, Tuple
 
 
-
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a tuple of the key and value
