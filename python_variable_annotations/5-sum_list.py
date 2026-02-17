dee27f0bb2b8d930bee9bc0007a6b8e812f3688a
NSF9 <alzhraniii999@gmail.com>
2026-02-18 00:22:18 +0300

    Add blank lines for improved readability in sum_list function

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """5-sum_list.py: Annotate the sum_list function with the correct types."""
 
-
 from typing import List
+
+
 def sum_list(input_list: List[float]) -> float:
     """Returns the sum of a list of floats."""
     return sum(input_list)
