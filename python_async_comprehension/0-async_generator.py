f9351d13d3198be6fdfcbe70e8e21123ad558387
NSF9 <alzhraniii999@gmail.com>
2026-02-21 21:18:16 +0300

    Fix docstring by adding missing period at the end of async_generator function

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,7 +6,7 @@ import random
 
 
 async def async_generator():
-    """Yield a random number between 0 and 10, 10 times, with 1s delay"""
+    """Yield a random number between 0 and 10, 10 times, with 1s delay."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
