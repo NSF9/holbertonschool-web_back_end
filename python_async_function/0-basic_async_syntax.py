074a9b96a29d382cb3d636e2759cd53f1a0a3034
NSF9 <alzhraniii999@gmail.com>
2026-02-21 20:35:54 +0300

    Add docstring to wait_random function for clarity on return value

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -6,6 +6,10 @@ from turtle import delay
 """async / await"""
 
 async def wait_random(max_delay: int = 10) -> float:
+
+    """Returns a random delay between 0 and max_delay (included)"""
+
+
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
