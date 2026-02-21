94587fb5da765bcb18f30d7d4604d5683cae4586
NSF9 <alzhraniii999@gmail.com>
2026-02-21 21:11:20 +0300

    Add async comprehension module and runtime measurement functionality

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,20 @@
 #!/usr/bin/env python3
-"""Module for async comprehension"""
+"""Module that defines an async comprehension coroutine.
+
+This module imports async_generator and defines a coroutine
+that collects 10 random numbers using an asynchronous
+comprehension.
+"""
 
 from typing import List
+
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Collects 10 random numbers using async comprehension"""
+    """Return a list of 10 random floats from async_generator.
+
+    Uses an asynchronous comprehension to iterate over
+    async_generator and collect the yielded values.
+    """
     return [number async for number in async_generator()]
