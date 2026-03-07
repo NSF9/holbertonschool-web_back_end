a353119fa3182b3b388118d48240667dbae04e26
Nawaf <Alzhraniii999@gmail.com>
2026-03-07 21:49:20 +0300

    update topic

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,9 @@
+""" pymongo
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ function that returns the list of school
+        having a specific topic
+    """
+    return mongo_collection.find({"topics": topic})
